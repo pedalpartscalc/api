f781ed03852c4250c77d290aef16cbc0e6b700b9
Jonathan Bayless <jonathanbayless@curative.com>
2022-02-13 17:16:49 -0500

    allow same-origin requests

--- src/bin/api.rs
+++ src/bin/api.rs
@@ -62,9 +62,36 @@ fn index() -> String {
     format!("Parts {:?}", parts::create_example_parts_list())
 }
 
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
+
+// From https://stackoverflow.com/a/69342225
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Attaching CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount(
+    rocket::build().attach(CORS).mount(
         "/",
         routes![index, get_parts, get_part, new_part, delete_part],
     )
