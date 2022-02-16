3bcb85a078adf186a2b94553b73520d65052f26c
Jonathan Bayless <jonathanbayless@curative.com>
2022-02-16 18:57:38 -0500

    switch to actix

--- migrations/2022-02-08-022600_create_available_parts/up.sql
+++
@@ -1,8 +0,0 @@
--- Your SQL goes here
-create table available_parts (
-  id bigserial primary key,
-  owner_id bigint not null,
-  part_name varchar not null,
-  part_kind varchar not null,
-  quantity integer not null
-);
--- src/schema.rs
+++
@@ -1,9 +0,0 @@
-table! {
-    available_parts (id) {
-        id -> Int8,
-        owner_id -> Int8,
-        part_name -> Varchar,
-        part_kind -> Varchar,
-        quantity -> Int4,
-    }
-}
