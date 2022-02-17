287930a1b54bea094807704385d786bb8443b04e
Jonathan Bayless <jonathanbayless@curative.com>
2022-02-17 13:12:00 -0500

    reorganization

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use pedalpartscalc::configuration::get_configuration;
 use pedalpartscalc::startup::run;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
