0efa344b3627e3cf32a17de44b6ae7e0ae15a7ee
Jonathan Bayless <jonathanbayless@curative.com>
2022-02-16 20:38:36 -0500

    reorganize files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use pedalpartscalc::run;
+use pedalpartscalc::configuration::get_configuration;
+use pedalpartscalc::startup::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    run(listener)?.await
 }
