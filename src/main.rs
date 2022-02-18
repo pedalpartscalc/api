c08377558ecf3d4f080d06a99cdb9e3b845d1641
Jonathan Bayless <jonathanbayless@curative.com>
2022-02-18 17:14:34 -0500

    up to end of chapter 7

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,37 @@
 use pedalpartscalc::configuration::get_configuration;
+use pedalpartscalc::email_client::EmailClient;
 use pedalpartscalc::startup::run;
 use pedalpartscalc::telemetry::{get_subscriber, init_subscriber};
-use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("pedalpartscalc".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool =
-        PgPool::connect(&configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres.");
-    // Here we choose to bind explicitly to localhost, 127.0.0.1, for security
-    // reasons. This binding may cause issues in some environments. For example,
-    // it causes connectivity issues running in WSL2, where you cannot reach the
-    // server when it is bound to WSL2's localhost interface. As a workaround,
-    // you can choose to bind to all interfaces, 0.0.0.0, instead, but be aware
-    // of the security implications when you expose the server on all interfaces.
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
+    // Build an `EmailClient` using `configuration`
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+    let timeout = configuration.email_client.timeout();
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout,
+    );
+
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await?;
+    run(listener, connection_pool, email_client)?.await?;
     Ok(())
 }
