4284451d76aca80cf39935f7d79d587b8a882c28
Jonathan Bayless <jonathanbayless@curative.com>
2022-02-22 20:33:22 -0500

    start with the Auth0 template

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
-use pedalpartscalc::configuration::get_configuration;
-use pedalpartscalc::startup::Application;
+mod api;
+mod extractors;
+mod middlewares;
+mod types;
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let application = Application::build(configuration).await?;
-    application.run_until_stopped().await?;
-    Ok(())
+use actix_web::{App, HttpServer};
+use dotenv::dotenv;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    env_logger::init();
+    let config = types::Config::default();
+    let auth0_config = extractors::Auth0Config::default();
+    HttpServer::new(move || {
+        App::new()
+            .app_data(auth0_config.clone())
+            .wrap(middlewares::cors(&config.client_origin_url))
+            .wrap(middlewares::err_handlers())
+            .wrap(middlewares::security_headers())
+            .wrap(middlewares::logger())
+            .service(api::routes())
+    })
+    .bind((config.host, config.port))?
+    .run()
+    .await
 }
